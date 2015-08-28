e223648392a050e61315148f96651cb8760ff017
Pepe Padial <jose.carlos.padial@gmail.com>
2015-08-28 16:32:46 +0200

    Generate new number on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,20 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(99)
+OK_MSG = "OK, generating new number"
 
 def check_guess(guess)
   return "" if guess.nil?
   guess = guess.to_i
   dif = guess - SECRET_NUMBER
-  return "OK" if dif == 0
-  return "#{dif <  5 ? "Too" : "Extremely"} high" if dif > 0
-  return "#{dif > -5 ? "Too" : "Extremely"} low" if dif < 0
+  return OK_MSG if dif == 0
+  return "#{dif <  5 ? "too" : "extremely"} high" if dif > 0
+  return "#{dif > -5 ? "too" : "extremely"} low" if dif < 0
 end
 
 get '/' do
-  message = check_guess(params["guess"])
+  guess = params["guess"]
+  message = check_guess(guess)
   number = 'XX'
-  number = SECRET_NUMBER if message == 'OK'
+  number = SECRET_NUMBER if message == OK_MSG
+  SECRET_NUMBER = rand(99) if message == OK_MSG
 
-  erb :index, :locals => { number: number, message: message }
+  erb :index, :locals => { number: number, message: message, guess: guess }
 end
